fc5f285c72858e8c13c9a1290c9d014edc8ef027
RenaarsA <DogieLV@Gmail.com>
2022-09-22 13:04:20 +0300

    y

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 async function iegutDarglietasNoApi() {
-let datiNoApi=await fetch('adrese')
+let datiNoApi=await fetch('https://fakestoreapi.com/products')
 let datiJson=datiNoApi.json;
         console.log(datiJson)
 }
