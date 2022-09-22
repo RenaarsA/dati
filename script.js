32a1dc22dce1e616f8f42c9288b0235fd41a03ff
RenaarsA <DogieLV@Gmail.com>
2022-09-22 13:08:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ let datiNoApi=await fetch('https://fakestoreapi.com/products')
 let datiJson=datiNoApi.json;
         console.log(datiJson)
 }
+
+async function iegutWaifu() {
+        let datiNoApi=await fetch('https://api.waifu.pics/many/type/category')
+        let datiJson=datiNoApi.json;
+                console.log(datiJson)
+        }
